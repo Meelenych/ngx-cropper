17420046961ef4d7f190acdf453cd12aa885cde9
Taras Demchenko <80649931+Meelenych@users.noreply.github.com>
2022-08-02 16:38:00 +0300

    cropper example used

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { ImageCroppedEvent, LoadedImage } from 'ngx-image-cropper';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngx-cropper';
+
+
+  imageChangedEvent: any = '';
+  croppedImage: any = '';
+  image: any = '';
+
+  fileChangeEvent(event: any): void {
+    this.imageChangedEvent = event;
+  }
+  imageCropped(event: ImageCroppedEvent) {
+    this.croppedImage = event.base64;
+  }
+  imageLoaded(image: LoadedImage) {
+    // show cropper
+  }
+  cropperReady() {
+    // cropper ready
+  }
+  loadImageFailed() {
+    // show message
+  }
 }
+
